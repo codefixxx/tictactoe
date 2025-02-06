dccb13664a0574fdcab9681d5d9df5170c3304a4
codefixx <ayushkmishra332@gmail.com>
2025-02-06 12:06:50 +0530

    all functionality of tictactoe is present except the logic for Draw. It will be added soon.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector("reset-btn");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-game")
+let msgContainer = document.querySelector(".msg-container")
+let msg = document.querySelector("#msg");
+
 let turnO = true;
 let winning_conditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 boxes.forEach((box) => {
@@ -14,5 +18,55 @@ boxes.forEach((box) => {
             turnO = true;
         }
         box.disabled = true;
+        checkWinner();
     })
 })
+const disableBoxes = () => {
+    for (box of boxes) {
+        box.disabled = true;
+    }
+}
+const enableBoxes = () => {
+    for (box of boxes) {
+        box.disabled = false;
+    }
+}
+newGameBtn.addEventListener("click", function reset() {
+    msgContainer.classList.add("hide");
+    turnO = true;
+    boxes.forEach(box => { box.innerText = ""; })
+    enableBoxes();
+
+})
+resetBtn.addEventListener("click", function reset() {
+    msgContainer.classList.add("hide");
+    turnO = true;
+    boxes.forEach(box => { box.innerText = ""; })
+    enableBoxes();
+
+})
+
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratualtions, winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+function checkWinner(){
+    for (condition of winning_conditions) {
+        let pos1val = boxes[condition[0]].innerText;
+        let pos2val = boxes[condition[1]].innerText;
+        let pos3val = boxes[condition[2]].innerText;
+
+        if (pos1val != "" && pos2val != "" && pos3val != "") {
+            if (pos1val === pos2val &&pos2val === pos3val) {
+                console.log("winner", pos1val);
+                showWinner(pos1val);
+                break;
+            }   
+        }
+    }
+
+
+}
